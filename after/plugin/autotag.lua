48b9cc46807007b6c04bd5eccb68cd5bec766e11
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2024-05-25 23:28:46 +0100

    Update autotag setup

--- after/plugin/autotag.lua
+++ after/plugin/autotag.lua
@@ -1,8 +1,9 @@
 require('nvim-ts-autotag').setup(
     {
-        enable = true,
-        enable_rename = true,
-        enable_close = true,
-        filetypes = { "htmx", "jsx", "html", "javascript", "tsx", "typescript", "xml" }
+        opts = {
+            enable = true,
+            enable_rename = true,
+            enable_close = true,
+        },
     }
 )
